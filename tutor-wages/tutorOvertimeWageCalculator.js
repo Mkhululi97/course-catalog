27db14df594a0d125fd073ff1272a325da32971f
mkhululi <mkhululimtshali@gmail.com>
2023-04-20 15:39:45 +0200

    updated the Presentation layer, stopped halfway on the turtor over time function

--- tutor-wages/tutorOvertimeWageCalculator.js
+++ tutor-wages/tutorOvertimeWageCalculator.js
@@ -1,3 +1,10 @@
-function tutorOvertimeWageCalculator(what) {
-  return what;
+function tutorOvertimeWageCalculator(timesheet, level) {
+  console.log(timesheet, level);
+  let hours = timesheet.split("-");
+  let totalHours = 0;
+  for (let i = 0; i < hours.length; i++) {
+    totalHours += Number(hours[i]);
+  }
+  let overTimePay = totalHours * 75 * 0.1;
+  console.log(overTimePay);
 }
